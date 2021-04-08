b87a95ec5b604a9c99b1e76f0486c3f85c926d9c
Jacob Moya <jemoya1994@gmail.com>
2021-04-07 21:10:46 -0700

    added comments for myself for tomorrow/readability

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
+// setting a number to one for now
 let number = 1;
 
+// executor function for promise
 let execFunc = (resolve,reject) =>{
+    // if statement to determine outcome 
     if(number ==1){
         resolve('were good');
     } else{
@@ -8,16 +11,19 @@ let execFunc = (resolve,reject) =>{
     }
 }
 
+// resolve function, to log the value returned within resolve
 const resolveFunc = (resval) =>{
     console.log(resval);
 }
 
+// resolve function, to log the value returned within reject
 const rejectFunc = (rejval) =>{
     console.log(rejval);
 }
-
+// invoke a promise
 let prom = new Promise(execFunc); 
 
+//using then & catch to resolve or reject
 prom
     .then(resolveFunc)
     .catch(rejectFunc);
