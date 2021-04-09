f656f0c4c47189e98054d031fc4edc475f92f106
Jacob Moya <jemoya1994@gmail.com>
2021-04-08 21:45:38 -0700

    Adding in some work currently being done and learned, next will be more asyn/await practice included

---
+++ async.js
@@ -0,0 +1,16 @@
+// practicing some async programing
+const numberadder = async (number1,number2) =>{
+    if(number1+number2 > 5){
+        return 'num is larger than 5';
+    } else {
+        return 'num is less than 5';
+    }
+}
+
+numberadder(1,4)
+    .then((resolveValue)=>{
+            console.log(resolveValue);
+    })
+
+// using online resources while learning as well, 
+// just getting into async await into more detail! I will master Javascript within the month neforebfore moving on
